ee5bb4a753bc3daebe15439dc4fb2ae9306ab108
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2025-01-19 12:52:27 +0530

    Create nonsystem_table.sql

---
+++ nonsystem_table.sql
@@ -0,0 +1,24 @@
+SELECT
+    table_schema,
+    table_name,
+    table_type,
+    engine,
+    table_rows,
+    avg_row_length,
+    data_length,
+    max_data_length,
+    index_length,
+    data_free,
+    auto_increment,
+    create_time,
+    update_time,
+    table_comment
+FROM
+    information_schema.tables
+WHERE
+    table_type NOT LIKE '%VIEW%'
+        AND
+    table_schema NOT IN ('mysql', 'sys', 'information_schema', 'performance_schema')
+ORDER BY
+    table_schema,
+    table_name;
